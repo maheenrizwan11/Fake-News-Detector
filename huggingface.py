79c3ac7e2c0164bf411efe2753df9616395b22be
Maheen Rizwan <maheenrizwanfya@gmail.com>
2025-07-17 02:34:20 +0500

    the final thing w real/fake detection with 63.2% accuracy and put the model on huggingface

--- huggingface.py
+++ huggingface.py
@@ -1,11 +1,7 @@
-from huggingface_hub import create_repo, upload_folder
+from huggingface_hub import upload_folder
 
-# Only run this ONCE to create the repo
-# create_repo("fake-news-bert", private=False)  # or private=True
-
-# Upload your model folder
 upload_folder(
-    folder_path="models/bert_fake_news_model",     # make sure this path is correct
-    repo_id="maheen21/fake-news-bert",         # your Hugging Face username/repo
+    folder_path="models/bert_fake_news_model",    
+    repo_id="maheen21/fake-news-bert",        
     repo_type="model"
 )
